6648426e64e2c62df5bddd83da111704ee044343
Sid Shelke <ausv2027@gmail.com>
2023-07-30 13:49:05 +0530

    default dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         </head>
       <body className={inter.className}>
         <ModalProvider />
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
         </body>
