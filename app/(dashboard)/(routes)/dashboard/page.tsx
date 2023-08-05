5034528917fb59b265102e53117ab5406492cd24
Sid Shelke <ausv2027@gmail.com>
2023-08-05 11:48:16 +0530

    margin fix

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -88,7 +88,7 @@ const DashBoardPage = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 overflow-x-hidden lg:grid-cols-3 md:gap-6 gap-2  px-6 selection:bg-white selection:text-purple-500">
+      <div className="grid grid-cols-1 overflow-x-hidden lg:grid-cols-3 md:gap-6 gap-2  px-3 pl-1 selection:bg-white selection:text-purple-500">
         {items.map((item) => {
           return (
             <Card
