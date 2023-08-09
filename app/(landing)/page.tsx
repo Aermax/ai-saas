9eea86d34f8a5608fc5796be871079b82a7c416b
Sid Shelke <ausv2027@gmail.com>
2023-08-09 18:06:18 +0530

    theme change

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -103,27 +103,6 @@ export default function LandingPage() {
           Sign In Today.
         </div>
       </div>
-
-      {/* <div className="px-10 pb-20 h-full bg-[#111827]">
-      <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {testimonials.map((item) => (
-          <Card key={item.description} className="bg-[#192339] border-none text-white">
-            <CardHeader>
-              <CardTitle className="flex items-center gap-x-2">
-                <div>
-                  <p className="text-lg">{item.name}</p>
-                  <p className="text-zinc-400 text-sm">{item.title}</p>
-                </div>
-              </CardTitle>
-              <CardContent className="pt-4 px-0">
-                {item.description}
-              </CardContent>
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
-    </div> */}
     </div>
   );
 }
